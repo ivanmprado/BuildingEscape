64cb01b495486b3748ad0def77b040ecf4596f35
Ivan Monteiro Prado <ivanmonteiroprado@gmail.com>
2017-01-12 16:26:14 -0200

    Initial commit

---
+++ Section_03/BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -0,0 +1,43 @@
+// Copyright Ivan Monteiro Prado 2017
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void OpenDoor();
+
+	void CloseDoor();
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	float DoorCloseDelay = 1.f;
+
+	float LastDoorOpenTime;
+
+	AActor* ActorThatOpens;
+	AActor* Owner;
+	
+};
